8decca439a6ac6fee10ade0c6f924c7e1f461567
lma0kek0zavr <101475590+lma0kek0zavr@users.noreply.github.com>
2023-04-20 17:01:08 +0300

    Add files via upload

---
+++ client/src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Sidebar } from './sidebar';
+export { default as Navbar } from './navbar';
+export { default as CustomButton } from './custombutton';
+export { default as FormField } from './formfield';
+export { default as DisplayCampaigns } from './displaycampaigns';
+export { default as FundCard } from './fundcard';
+export { default as CountBox } from './countbox';
+export { default as Loader } from './loader';
---
+++ client/src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from './home';
+export { default as Profile } from './profile';
+export { default as CampaignDetails } from './campaigndetails';
+export { default as CreateCampaign } from './createcampaign';
